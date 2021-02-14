0da94e2a3281e902623ed9fcfd6c17ff7b900d40
Erasmo1015 <2675325968@qq.com>
2021-02-14 16:41:41 +0800

    Eval example

--- utils/evaluate_l1norm.py
+++ utils/evaluate_l1norm.py
@@ -1,4 +1,6 @@
 import torch
+from torchvision.models import resnet18
+import data_loader_l1norm
 
 def eval(model, test_loader):
     correct = 0
@@ -16,4 +18,23 @@ def eval(model, test_loader):
             total += len(target)
     return correct / total
 
+if __name__ == '__main__':
+    model = resnet18(pretrained=True)
+    train_loader, test_loader = data_loader_l1norm.get_dataloader()
     
+    correct = 0
+    total = 0
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.eval()
+    with torch.no_grad():
+        for i, (img, target) in enumerate(test_loader):
+            img = img.to(device)
+            out = model(img)
+            pred = out.max(1)[1].detach().cpu().numpy()
+            target = target.cpu().numpy()
+            correct += (pred==target).sum()
+            total += len(target)
+            break
+    acc = correct / total
+    print("Acc=%.4f\n"%(acc))
